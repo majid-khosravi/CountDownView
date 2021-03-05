13ceb902015799196fb67588dd0056233d47ccf6
Majid <m.khosravi@farakav.com>
2021-03-05 14:42:07 +0330

    The INVALID_VALUE defined.

--- countdown/src/main/java/ir/khosravi/countdownview/utils/DimenUtils.java
+++ countdown/src/main/java/ir/khosravi/countdownview/utils/DimenUtils.java
@@ -9,15 +9,17 @@ import android.content.res.Resources;
 
 public class DimenUtils {
 
+    public static int INVALID_VALUE = -1;
+
     private DimenUtils() {
     }
 
     public static float dp2px(Resources resources, float dp) {
         final float scale = resources.getDisplayMetrics().density;
-        return  dp * scale + 0.5f;
+        return dp * scale + 0.5f;
     }
 
-    public static float sp2px(Resources resources, float sp){
+    public static float sp2px(Resources resources, float sp) {
         final float scale = resources.getDisplayMetrics().scaledDensity;
         return sp * scale;
     }
